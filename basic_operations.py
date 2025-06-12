074d83502b1cb8e442ff1828d5ce64227ed5446c
Yonatan H <yenetaneyh@gmail.com>
2025-06-12 10:16:31 +0300

    Delete basic_operations.py
    deleted

--- basic_operations.py
+++
@@ -1,8 +0,0 @@
-number1 = 10 
-number2 = 5
-addition = number1 + number2
-substraction = number1 - number2
-multiplication = number2 * number1
-print("Addition of" , number1 , "and" , number2 , "is" , addition)
-print("Subtraction of" , number1 , "and" , number2 , "is" , substraction)
-print("Multiplication of" , number1 , "and" , number2 , "is" , multiplication)
