ed8e5b213e667510883a3e204ed81c580eff0fc7
Yonatan Haile <yenetaneyh@gmail.com>
2025-06-17 12:20:08 +0300

    updated

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,5 @@
 # math_operations.py
-def perform_operation (num1, num2, operation) -> float:
+def perform_operation(num1: float, num2: float, operation: str) -> float:
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
