31d12547f5380f4204888a33c42d7f85c037a47f
Yonatan Haile <yenetaneyh@gmail.com>
2025-06-23 17:19:12 +0300

    .

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -5,22 +5,23 @@ class Book:
         self.is_checked_out = False
 
 class Library:
-    _books = []
+    def __init__(self):
+        self._books = []
     def add_books(self,book):
         self.book = book
-        self._books.append(Book)
+        self._books.append(book)
     def check_out_book(self, title):
         for book in self._books:
-            if Book.title == title and not Book.is_checked_out:
-                Book.is_checked_out = True
-                print(f"Checked out: '{Book.title}' by '{Book.author}'")
+            if book.title == title and not book.is_checked_out:
+                book.is_checked_out = True
+                print(f"Checked out: '{book.title}' by '{book.author}'")
                 return
         print(f'Book not available: "{title}"')
      
     def return_book(self, title):
         for book in self._book:
-            if Book.title == title and not Book.is_checked_out():
-                Book.return_book()
+            if book.title == title and not book.is_checked_out():
+                book.return_book()
                 print(f"returned :{title}")
                 return
         print(f"Book not found or not cheaked out :{title}")
@@ -28,5 +29,5 @@ class Library:
     def list_available_books(self):
         print("Avaliable books: ")
         for book in self._books:
-            if Book.is_checked_out():
-                print(f"{Book.title} by {Book.author}")
+            if book.is_checked_out():
+                print(f"{book.title} by {book.author}")
