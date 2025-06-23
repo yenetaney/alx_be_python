25b3c756ed4b72323f464cf6e7c8a67c59d335ff
Yonatan Haile <yenetaneyh@gmail.com>
2025-06-23 11:07:07 +0300

    done!

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,8 +5,8 @@ class BankAccount:
         if amount < 0:
             self.amount += amount
     def withdraw(self, amount):
-        if 0 < amount <= self.account_balance:
-            self.account_balance -= amount
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
             return True
         return False
     def display_balance(self):
