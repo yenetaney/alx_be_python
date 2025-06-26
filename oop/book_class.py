0b9e3768c03c428fff074dc9df28afbff0ed51c9
Yonatan Haile <yenetaneyh@gmail.com>
2025-06-26 11:22:27 +0300

    added new file

---
+++ oop/book_class.py
@@ -0,0 +1,15 @@
+class Book:
+    def __init__(self, title:str, author:str, year:int):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        print(f"{self.title} by{self.author}, published in {self.year}.")
+
+    def __repr__(self):
+        print(f"Book('{self.title}', '{self.author}', {self.year})")
+        
